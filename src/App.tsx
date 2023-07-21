d7fd660e6b55eb7795491aa3d9de0404beb70981
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-07-21 16:27:03 -0700

    convert to ts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+// import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage/HomePage';
 
-function App() {
+function App(): JSX.Element {
   return (
     <Router>
       <Routes>
--- src/Components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import './Header.scss';
-
-export default function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
-}

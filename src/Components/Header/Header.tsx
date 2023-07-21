165651787392f7f25b24f725d308394b87dcb972
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-07-21 16:40:13 -0700

    improve code.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,12 +1,11 @@
 // import React from 'react';
 import './Header.scss';
 
-const Header: React.FC = () => {
-  return (
-    <div>
-      Beep
-    </div>
-  );
-};
+const Header: React.FC = () => (
+  <div>
+    Beep
+  </div>
+);
+
 
 export default Header;

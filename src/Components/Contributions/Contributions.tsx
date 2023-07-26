ae8f1948c79c07ee951aa337bb7e5848b1fc4c66
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-07-26 11:13:58 -0700

    clean code.

--- src/Components/Contributions/Contributions.tsx
+++ src/Components/Contributions/Contributions.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchContributions, ContributionData } from '../../utils/github';
 import './Contributions.scss';
 

2be89a07dc67da0d242c6232274bd377c50818c3
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-07-30 21:12:52 -0700

    add markup for homepage.

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import './HomePage.scss';
 import { useDocumentTitle } from '../../utils/functions';
-import { animateButton } from '../../utils/elements';
+// import { animateButton } from '../../utils/elements';
 import Contributions from '../../Components/Contributions/Contributions';
 import Repositories from '../../Components/Repositories/Repositories';
 
@@ -9,19 +9,15 @@ const HomePage: React.FC = () => {
 
   return (
     <>
-      <div className='divider'>
-        <span className="dividerd"></span>
-        <ul className="fun">
-          <li className="fun__item" onClick={animateButton}>01</li>
-          <li className="fun__item" onClick={animateButton}>02</li>
-          <li className="fun__item" onClick={animateButton}>03</li>
-          <li className="fun__item" onClick={animateButton}>04</li>
-        </ul>
 
-        <p className="font writing-block">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi iusto praesentium odit iure omnis hic fugiat ipsum ad nihil deserunt officiis nisi laborum nesciunt impedit deleniti optio ea adipisci voluptatem, inventore ducimus! Vero itaque voluptatum fugit omnis qui tempore provident pariatur dolores cupiditate? Error, exercitationem. Exercitationem impedit sunt, ea vero ipsa eligendi! Tenetur magnam animi accusantium dolor neque explicabo facere, perspiciatis cum eum nam voluptates fugit veniam corporis repellendus ab quod illo esse repudiandae porro praesentium! Ad rerum nobis consectetur sed mollitia, ullam harum quis debitis dolorem, maiores impedit amet architecto, laborum non. Inventore nulla esse placeat similique ducimus repellat.</p>
-
-
+      <div id="portfolio-left">
+        <section className="info">
+          <h1 className="info__title">Philip Lucas</h1>
+          <h2 className="info__subtitle">Full-Stack Web Developer</h2>
+          <p className="info__description">I build modern websites with user interaction and efficiency in mind.</p>
+        </section>
       </div>
+
       <Contributions />
 
       <Repositories />

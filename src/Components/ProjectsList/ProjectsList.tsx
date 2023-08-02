e8724b52cf85c2e5995aec6ab44fee1114f5951c
stuckinsnow <126236947+stuckinsnow@users.noreply.github.com>
2023-08-02 11:46:45 -0700

    add automatic styling for tech names.

--- src/Components/ProjectsList/ProjectsList.tsx
+++ src/Components/ProjectsList/ProjectsList.tsx
@@ -2,16 +2,8 @@ import './ProjectsList.scss';
 import projectData from '../../data/projects';
 import { Link } from 'react-router-dom';
 
-
-// projectData.technology.forEach(element => {
-//     const text = projectData.technology.textContent;
-//     if (text) {
-//         projectData.t
-//     }
-// });
-
-
 const ProjectsList: React.FC = () => {
+
     return (
 
         <div id="c-projects" className='projects'>
@@ -29,7 +21,11 @@ const ProjectsList: React.FC = () => {
                     <div className='projects-card__group'>
                         <div className='projects-card__item--double'>{projectData.type} <span>{projectData.date}</span></div>
                         <div className='projects-card__item'>{projectData.desc}</div>
-                        <div className='projects-card__item'>{projectData.technology.join(", ")}</div>
+                        <div className='projects-card__item projects-card__tech'>
+                            {projectData.technology.map((tech, index) => (
+                                <span key={index} className={'projects-card__tech--list' + ' ' + 'projects-card__tech--' + tech.toLowerCase().replace(/[.\s]+/g, '-')} >{tech}</span>
+                            ))}
+                        </div>
                     </div>
 
                 </section>

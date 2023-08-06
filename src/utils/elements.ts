de85fbbf7a79a99c29dd65aa12fb25fed208df0c
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-08-06 12:08:18 -0700

    separate functions.

--- src/utils/elements.ts
+++ src/utils/elements.ts
@@ -16,3 +16,32 @@ export const animateButton = (e: React.MouseEvent<HTMLLIElement>) => {
         target.removeChild(ripple);
     }, 300);
 };
+
+// utils.ts
+export const toggleActiveClass = (index: number, setActiveProjectIndex: Function) => {
+    setActiveProjectIndex((prevIndex: number | null) => (prevIndex === index ? null : index));
+};
+
+export const handleActiveProject = (
+    activeProjectRef: React.RefObject<HTMLElement>,
+    activeProjectIndex: number | null
+) => {
+    if (activeProjectRef.current && activeProjectIndex !== null) {
+        const activeDiv = activeProjectRef.current;
+        const dataIndex = parseInt(activeDiv.dataset.index || '');
+        if (activeProjectIndex === dataIndex) {
+            // Calculate the actual height of the content and set it rather than an "auto" value--auto values don't allow transitions, and "max-height" has problems of its own e.g. if the actual height is 10rem but max-height is set to 100rem, the transition will be off for the remaining 90rem
+            const height = activeDiv.scrollHeight;
+            activeDiv.style.maxHeight = `${height}px`;
+            activeDiv.classList.add('expanded');
+        } else {
+            const height: any = activeDiv.scrollHeight; // Set type to 'any'
+            activeDiv.style.maxHeight = height;
+            activeDiv.classList.remove('expanded');
+        }
+    }
+};
+
+
+
+

3fa2bbc5de9d465cb58493eec41767ff1c91859d
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-08-08 22:01:24 -0700

    add graph to front page.

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import './HomePage.scss';
 import { useDocumentTitle } from '../../utils/functions';
 import { Link } from 'react-router-dom';
+import Contributions from '../../Components/Contributions/Contributions';
 
 const HomePage: React.FC = () => {
   useDocumentTitle('Home Page');
@@ -12,11 +13,14 @@ const HomePage: React.FC = () => {
           <h1 className="info__title">Name Here</h1>
           <h2 className="info__subtitle">Full-Stack Web Developer</h2>
           <p className="info__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptate ratione nisi, libero earum ut ducimus inventore velit optio! Expedita.</p>
+
+          <Contributions />
         </section>
 
-        <div className="links">
+        {/* <div className="links">
           <Link to="/projects" className="links__link">Projects</Link>
-        </div>
+          <Link to="/about" className="links__link">About</Link>
+        </div> */}
 
       </div>
 

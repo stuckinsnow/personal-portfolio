30633c7b2bc4483b1d02575a78c3bc03b6cf3f73
Philip <126236947+stuckinsnow@users.noreply.github.com>
2023-08-19 09:07:15 -0700

    remove unused page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-// import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Header from './Components/Header/Header';
 // import HomePage from './Pages/HomePage/HomePage';
-import Calculator from './Pages/Calculator/Calculator';
-// import Footer from './Components/Footer/Footer';
 import ProjectsPage from './Pages/ProjectsPage/ProjectsPage';
 
 function App() {
   return (
     <Router>
-      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<ProjectsPage />} />
-        <Route path="/calculator" element={<Calculator />} />
         <Route path="/projects" element={<ProjectsPage />} />
       </Routes>
-      {/* <Footer /> */}
     </Router>
   );
 }

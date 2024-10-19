8154a0e8a05f261bc96cf96cbfb398f64bb83534
Philip <126236947+stuckinsnow@users.noreply.github.com>
2024-10-19 23:32:48 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import HomePage from './Pages/HomePage/HomePage';
 import SnippetsPage from './Pages/SnippetsPage/SnippetsPage';
 import WobbleButton from './Pages/SnippetsPage/WobbleButton/WobbleButton';
 
 function App() {
+
+  useEffect(() => {
+    // Create a script element
+    const script = document.createElement('script');
+
+    // Set the attributes for the script
+    script.src = "https://umami-wwows8g84w0g40gc0k88cgwg.184.73.178.248.sslip.io/script.js";
+    script.setAttribute('data-website-id', '0ecf9fe8-7745-4391-910f-c815185f208b');
+    script.defer = true; // Ensure it loads after the HTML is parsed
+
+    // Append the script to the body
+    document.body.appendChild(script);
+
+    // Cleanup function to remove the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []); // Empty dependency array ensures this runs only once when the component mounts
+
+
   return (
     <Router>
       <Routes>

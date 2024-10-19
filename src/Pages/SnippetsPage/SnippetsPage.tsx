98089e4d90d6a0b32ddf38071254eddbe5d36fdf
Philip <126236947+stuckinsnow@users.noreply.github.com>
2024-10-19 23:17:25 +0100

    Update SnippetsPage.tsx

--- src/Pages/SnippetsPage/SnippetsPage.tsx
+++ src/Pages/SnippetsPage/SnippetsPage.tsx
@@ -1,16 +1,6 @@
 import { Link } from 'react-router-dom';
 import './SnippetsPage.scss';
 
-import umami from '@umami/node';
-
-
-umami.init({
-    websiteId: '0ecf9fe8-7745-4391-910f-c815185f208b',
-    hostUrl: 'https://umami-wwows8g84w0g40gc0k88cgwg.184.73.178.248.sslip.io/'
-});
-
-umami.track({ url: 'https://expsr.com/snippets' });
-
 const SnippetsPage = () => {
     return (
         <div className='bebop'>

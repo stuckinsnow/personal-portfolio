c044d9a11ad924e818dc5014ced777cb9bf2a379
Philip <126236947+stuckinsnow@users.noreply.github.com>
2024-10-19 01:13:04 +0100

    Update SnippetsPage.tsx

--- src/Pages/SnippetsPage/SnippetsPage.tsx
+++ src/Pages/SnippetsPage/SnippetsPage.tsx
@@ -8,6 +8,10 @@ const SnippetsPage = () => {
 
             <Link to="/wobble-button">Wobble Button</Link>
 
+            <iframe src="https://app.polymuse.tech/embed?id=20240905190805" width="100%" height="500" data-bg="#FFFFFF" frameborder="0" allowfullscreen></iframe>
+            <iframe src="https://app.polymuse.tech/embed?id=Ideet" width="100%" height="500" data-bg="#FFFFFF" frameborder="0" allowfullscreen></iframe>
+            <iframe src="https://app.polymuse.tech/embed?id=20241009152627" width="100%" height="500" data-bg="#FFFFFF" frameborder="0" allowfullscreen></iframe>
+
         </div>
     )
 }
